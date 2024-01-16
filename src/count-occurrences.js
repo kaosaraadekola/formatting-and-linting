63bac53c66fd12437a9701cc0933ddb14696202e
Kaosara Adekola <kaosaraidris@hotmail.co.uk>
2024-01-16 23:46:54 +0000

    code updated

--- src/count-occurrences.js
+++ src/count-occurrences.js
@@ -9,15 +9,15 @@
  * @param {string} n - string to count occurrences off
  * @returns {number} how many occurrences of the string are in the array
  */
-function x(y, n) {
-  let x2 = 0;
-  for (let mars of y) {
-    if ((n = mars)) {
-      x2++;
+function occurrenceCount (stringOfArr, stringToTarget) {
+  let count = 0;
+  for (let string of stringOfArr) {
+    if ((stringToTarget === string)) {
+      count= count + 1;
     }
   }
 
-  return x2;
+  return count;
 }
 
 console.log(x(["na", "na", "na", "na", "BATMAN"], "na"), 4);
